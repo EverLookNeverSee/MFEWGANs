51cfe22e2379daf5122e1d0e4dc716e436d7fbac
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-09 20:54:18 +0430

    Defining discriminator class and implementing its initializer

--- main/discriminator.py
+++ main/discriminator.py
@@ -3,3 +3,22 @@
 """
 
 from torch import nn
+
+
+class Discriminator(nn.Module):
+    """ Implementation of discriminator """
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(64, 1),
+            nn.Sigmoid()
+        )
