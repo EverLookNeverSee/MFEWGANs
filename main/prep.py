9208b77c31e605e3f11ef02bc4727e82a3e9f47b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-09 20:02:26 +0430

    Training data preparation

--- main/prep.py
+++ main/prep.py
@@ -9,3 +9,13 @@ import matplotlib.pyplot as plt
 
 # Setting seed parameter
 torch.manual_seed(111)
+
+# Training data preparation
+train_data_length = 1024
+train_data = torch.zeros((train_data_length, 2))
+train_data[:, 0] = 2 * math.pi * torch.rand(train_data_length)
+train_data[:, 1] = torch.sin(train_data[:, 0])
+train_labels = torch.zeros(train_data_length)
+train_set = [
+    (train_data[i], train_labels[i]) for i in range(train_data_length)
+]
