7e8a4d814ea6a8e98484100dd45702546767beed
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-10 17:06:12 +0430

    Instantiating and setting up optimization algorithm

--- main/run.py
+++ main/run.py
@@ -1,4 +1,4 @@
-from torch import nn
+from torch import nn, optim
 from generator import Generator
 from discriminator import Discriminator
 
@@ -7,3 +7,12 @@ from discriminator import Discriminator
 lr = 0.001
 n_epochs = 300
 loss_function = nn.BCELoss()
+
+# Instantiating
+generator = Generator()
+discriminator = Discriminator()
+
+
+# Setting optimization algorithm
+optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=lr)
+optimizer_generator = optim.Adam(generator.parameters(), lr=lr)
