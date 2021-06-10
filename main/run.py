dddfc2ab3d18c9adbd7f110044603e472b3fbb76
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-10 17:02:01 +0430

    Setting learning rate, number of epochs and loss function

--- main/run.py
+++ main/run.py
@@ -1,2 +1,9 @@
+from torch import nn
 from generator import Generator
 from discriminator import Discriminator
+
+
+# Setting learning rate, number of epochs and loss function
+lr = 0.001
+n_epochs = 300
+loss_function = nn.BCELoss()
