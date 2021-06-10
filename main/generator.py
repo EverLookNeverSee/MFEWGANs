2bf1d399d13b6d7849553a1085e86bf25a20ab57
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-10 16:55:35 +0430

    Defining generator class and implementing its initializer

--- main/generator.py
+++ main/generator.py
@@ -3,3 +3,16 @@
 """
 
 from torch import nn
+
+
+class Generator(nn.Module):
+    """ Implementation of generator """
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 16),
+            nn.ReLU(),
+            nn.Linear(16, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2)
+        )
