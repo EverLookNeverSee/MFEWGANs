723d488951b8d6f570518e26e182074c1618345b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-11 21:49:09 +0430

    Defining generator class and its initializer

--- main/HRD_GAN/classes.py
+++ main/HRD_GAN/classes.py
@@ -27,3 +27,19 @@ class Discriminator(nn.Module):
         x = x.view(x.size(0), 784)
         output = self.model(x)
         return output
+
+
+class Generator(nn.Module):
+    """ Implementation of generator """
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(100, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
