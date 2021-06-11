763f5997f93f5b1ffc9836b7ec7736c69cb3b82e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-11 18:50:28 +0430

    Defining Discriminator class and its initializer

--- main/HRD_GAN/classes.py
+++ main/HRD_GAN/classes.py
@@ -1,3 +1,24 @@
 """
     This file contains the implementations of Generator and Discriminator classes
 """
+
+from torch import nn
+
+
+class Discriminator(nn.Module):
+    """ Implementation of discriminator """
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(784, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
