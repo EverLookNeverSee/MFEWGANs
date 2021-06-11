92ad0c1c7e1965ce58a6800e4d691a393fe57f5e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-11 17:58:49 +0430

    Setting gpu availability

--- main/HRD_GAN/main.py
+++ main/HRD_GAN/main.py
@@ -12,3 +12,9 @@ import torchvision.transforms as transforms
 
 # Setting up seed
 torch.manual_seed(111)
+
+device = ""
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
