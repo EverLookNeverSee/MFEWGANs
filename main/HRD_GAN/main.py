6558c55f5dfc09ad0cbd8dbe06a8abd8c432248a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-06-11 17:56:19 +0430

    Importing relevant libraries

--- main/HRD_GAN/main.py
+++ main/HRD_GAN/main.py
@@ -1,3 +1,10 @@
 """
     Generating hand-written digits using GANs
 """
+
+import math
+import torch
+import torchvision
+from torch import nn
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
